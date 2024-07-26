72c6de59570fa096fdef775b06b6705cf7288976
Ayush <ayushmitra06@gmail.com>
2024-07-26 22:53:00 +0530

    update index.js for a get request

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.use(cors());
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the server');
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
